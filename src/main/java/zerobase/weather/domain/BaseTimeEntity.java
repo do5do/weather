ed3c7e4a5a9b27be7865adcd5637121a46de12d2
do5do <do151015@gmail.com>
2023-10-25 12:23:03 +0900

    jpa auditing 설정 수정

--- src/main/java/zerobase/weather/domain/BaseTimeEntity.java
+++ src/main/java/zerobase/weather/domain/BaseTimeEntity.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@EnableJpaAuditing // 따로 설정을 빼둔 이유는 run application에 있으면 mock 객체 생성 시 실패함
 public class BaseTimeEntity {
     @CreatedDate
     LocalDateTime createdDate;
